e0d77abcdd6b511278eedd6e2505d92dd0d6d9c5
Radosław <57417179+rabranto@users.noreply.github.com>
2021-08-25 19:49:28 +0200

    Add class and methods

---
+++ ObjectOrientedProgramming/ObjectOrientedProgramming/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ObjectOrientedProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program save information about Farmer's animals in a ranch.");
+            Animal a1 = new();
+            a1.AddAnimal();
+            a1.Speak();
+        }
+    }
+    public class Animal
+    {
+        string animalType;
+        string name;
+        int age;       
+        
+
+        public void AddAnimal()
+        {
+            Console.Write("Adding animal to the database...\nWrite type of an Animal: ");
+            animalType = Console.ReadLine();
+            Console.Write("Write name of an Animal: ");
+            name = Console.ReadLine();
+            Console.Write("Write age of an Animal: ");
+            string readAge = Console.ReadLine();
+            int.TryParse(readAge, out age);
+        }
+
+        public void Speak()
+        {
+            if (animalType == "cat")
+            {
+                Console.WriteLine($"{name} lat {age}: Miauu!");
+            }
+            else if (animalType == "goat")
+            {
+                Console.WriteLine($"{name} lat {age}: Meee!");
+            }
+            else if (animalType == "cow")
+            {
+                Console.WriteLine($"{name} lat {age}: Muuu!");
+            }
+            else
+            {
+                Console.WriteLine("Unrecognizable animal type.");
+            }
+
+        }
+    }
+}
